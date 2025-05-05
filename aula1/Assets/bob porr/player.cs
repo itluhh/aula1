f3b6580093701185808adbdee786e19ff8afd1ae
itluhh <v.italo@escolar.ifrn.edu.br>
2025-05-05 12:08:30 -0300

    coisa

---
+++ aula1/Assets/bob porr/player.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private int numero = 4;
+
+    public float velocidade = 40;
+    
+    
+    void Start()
+    {//Debug. Log (numero)
+        {//Debug. Log (numero) = numero + 1
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
