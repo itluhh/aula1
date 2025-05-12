bf5cf9863f31dbe17a33eb36d3e4ffb6d2bd8b42
itluhh <v.italo@escolar.ifrn.edu.br>
2025-05-12 11:53:26 -0300

    codigos 1

--- aula1/Assets/bob porr/player.cs
+++ aula1/Assets/bob porr/player.cs
@@ -7,16 +7,27 @@ public class player : MonoBehaviour
     private int numero = 4;
 
     public float velocidade = 40;
-    
-    
+    // Start is called on the first execution of update after the MonoBehaviour is created
+
     void Start()
-    {//Debug. Log (numero)
-        {//Debug. Log (numero) = numero + 1
+    {
+        numero = 0;
     }
+    // update is called once per frame 
 
     void Update()
     {
-        
+        //Debug. Log (numero);
+        //Debug. Log (numero) = numero + 1;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        }
     }
 }
-
