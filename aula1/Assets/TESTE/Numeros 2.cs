38c3db604ae48b30e4d293af5bcb602883731694
itluhh <v.italo@escolar.ifrn.edu.br>
2025-05-26 11:54:07 -0300

    cu

---
+++ aula1/Assets/TESTE/Numeros 2.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Numeros2 : MonoBehaviour
+{
+    public int numero;
+
+    private bool resposta;
+    private int menor = 0;
+    private int maior = 10;
+    private int NumeroDeTentativas = 10;
+
+    private bool oJogoEstáRodando = true;
+    private Random rand = new Random();
+    private int sorteado; 
+    
+    
+    void Start()
+    {
+        sorteado = rand.Next(menor, maior + 1);
+
+    }
+
+}
+  
