05fddf88fffa29d9e10c61dce984dd14cb0228c2
itluhh <v.italo@escolar.ifrn.edu.br>
2025-05-27 08:28:52 -0300

    gru

--- aula1/Assets/TESTE/Numeros 2.cs
+++ aula1/Assets/TESTE/Numeros 2.cs
@@ -1,25 +1,45 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Numeros2 : MonoBehaviour
 {
-    public int numero;
+    public int Numero;
 
     private bool resposta;
     private int menor = 0;
     private int maior = 10;
     private int NumeroDeTentativas = 10;
 
-    private bool oJogoEstáRodando = true;
+    bool oJogoEstáRodando = true;
+
     private Random rand = new Random();
-    private int sorteado; 
-    
-    
+    private int sorteado;
+
     void Start()
     {
         sorteado = rand.Next(menor, maior + 1);
 
+        Debug.Log("Pensei em um numero entre '+menor+' e '+maior+', tente adivinhar");
     }
 
+    void Update()
+    {
+        //testa o numero quando aparte a tecla de espaço
+        if (Input.GetKeyDown(KeyCode.Space)) ;
+        {
+            Debug.Log("Você tem +NumeroDeTentativas+ 'tentativas!'");
+            int NUmeroDigitado = Numero;
+
+        }
+    }
+
+
+
+
+
+
+
 }
+
   
